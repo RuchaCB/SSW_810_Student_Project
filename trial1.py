e287c9356310e3ddab677c8bdf060cf198789281
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2019-04-10 22:38:04 -0400

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -24,20 +24,15 @@ class Repository():
     def __init__(self, dir):
         self.students = dict()
         self.instructors = dict()
-        self.majors = dict ()
+        self.majors = dict()
         self.add_students(os.path.join(dir, "students.txt"))
         self.add_instructors(os.path.join(dir, "instructors.txt"))
         self.add_grades(os.path.join(dir, "grades.txt"))
-        self.add_majors(os.path.join(dir, "majors.txt"))
+        self.add_major(os.path.join(dir, "majors.txt"))
+        self.add_courses()
+        self.major_pt()
         self.student_pt()
         self.instructor_pt()
-        self.major_pt()
-
-    def add_majors(self, major_file_path):
-        for dept, r_e, course in file_reader(major_file_path, 3, "\t"):
-            if dept not in self.majors:
-                self.majors[dept] = Major(dept)
-            self.majors[dept].add_course(course, r_e)
 
     def add_students(self, students_file_path):
         for s_cwid, s_name, major in file_reader(students_file_path, 3, "\t"):
@@ -54,12 +49,18 @@ class Repository():
             self.students[s_cwid].add_course(course, grade)
             self.instructors[i_cwid].add_course(course)
 
+    def add_major(self, major_file_path):
+        for dept, rore, course in file_reader(major_file_path, 3, "\t"):
+            if dept not in self.majors:
+                self.majors[dept] = Major(dept)
+            self.majors[dept].add_course(course, rore)
+    
     def major_pt(self):
         pt = PrettyTable(field_names=Major.fields())
         for major in self.majors.values():
             pt.add_row(major.details())
-        print(pt)
-  
+        print(pt)   
+
     def student_pt(self):
         print ('Student Summary')
         pt = PrettyTable(field_names = Student.fields())
@@ -75,6 +76,10 @@ class Repository():
                 pt.add_row(i)
         print(pt)
 
+    def add_courses(self):
+        for student in self.students.values():
+            student.courses, student.r_e = self.majors[student.major].get_courses(student.courses)
+
 class Major:
 
     def __init__(self, major):
@@ -82,16 +87,16 @@ class Major:
         self.courses = list()
         self.r_e = list()
 
-    def add_course(self, course, r_e):
-        if r_e == "R":
-            self.r_e.append(course)
-        elif r_e == "E":
+    def add_course(self, course, rore):
+        if rore == "R":
+            self.courses.append(course)
+        elif rore == "E":
             self.r_e.append(course)
 
-    def result(self, completed):
+    def get_courses(self, completed):
         result1 = []
         result2 = []
-        for course in self.r_e:
+        for course in self.courses:
             if course not in completed:
                 result1.append(course) 
         for elective in self.r_e:
@@ -99,8 +104,9 @@ class Major:
                 result2.append(elective)
             else:
                 result2 = None
-                break
+                break      
         return (result1, result2)
+
 
 
     def details(self):
@@ -111,27 +117,26 @@ class Major:
         return ["Dept", "Required", "Electives"]
 
 class Student:
-
-    def __init__(self, s_cwid, s_name, major):
+    
+    def __init__(self,s_cwid,s_name,major):
         self.s_cwid = s_cwid
         self.s_name = s_name
         self.major = major
-        self.courses = dict()
-        self.course = list()
+        self.course = dict()
+        self.courses = list()
         self.r_e = list()
-
-
-    def add_course(self, course, grades):
-        if grades in ["A", "A-", "B+", "B", "B-", "C"]:
-            self.courses[course] = grades
-
+    
+    def add_course(self,course,grade):
+        if grade in ['A', 'A-', 'B+', 'B', 'B-', 'C']:
+            self.courses[course] = grade
 
     def details(self):
-        return [self.s_cwid, self.s_name, self.major, sorted(self.courses.keys()), self.courses, self.r_e]
+        return [self.s_cwid, self.s_name, self.major, sorted(self.course.keys()), self.courses, self.r_e]
 
     @staticmethod
     def fields():
         return ["CWID", "Name", "Major", "Completed Courses", "Remaining Required", "Remaining Electives"]
+
     
 class Instructor:
 
@@ -150,9 +155,8 @@ class Instructor:
 
     @staticmethod
     def fields():
-        return["Cwid","name","dept","course","students"]
+        return ["CWID", "Name", "Dept", "Course", "Students"]
 
-'''
 class RepositoryTest(unittest.TestCase):
 
     def test_init(self):
@@ -165,11 +169,11 @@ class RepositoryTest(unittest.TestCase):
             result2[keys] = values.details()
 
         self.assertEqual(result1, result2)
-'''
+
     
 def main():
     ob = Repository("/Users/ruchabhatawadekar/Desktop/Stevens")
 
 if __name__ == '__main__':
-    #unittest.main(exit=False, verbosity=2)
+    unittest.main(exit=False, verbosity=2)
     main()
