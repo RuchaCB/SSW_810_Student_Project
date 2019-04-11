a986635cf058fa6e23b57753f7a51b0ed40dc9e1
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2019-04-11 06:07:38 -0400

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -1,6 +1,9 @@
-import unittest, os
+import datetime
+import os
+import unittest
 from collections import defaultdict
 from prettytable import PrettyTable
+import unittest
 
 def file_reader(path, num_fields, sep=",", header=False):
     try:
@@ -78,40 +81,6 @@ class Repository:
             pt.add_row(major.details())
         print(pt)
 
-class Major:
-    def __init__(self, major):
-        self.major = major
-        self.req = list()
-        self.elec = list()
-
-    def add_course(self, course, r_or_e):
-        if r_or_e == "R":
-            self.req.append(course)
-        elif r_or_e == "E":
-            self.elec.append(course)
-
-    def get_req(self, courses_completed):
-        result1 = []
-        result2 = []
-        for course in self.req:
-            if course not in courses_completed:
-                result1.append(course)
-        
-        for elective in self.elec:
-            if elective not in courses_completed:
-                result2.append(elective)
-            else:
-                result2 = "-"
-                break  
-        return (result1, result2)
-
-    def details(self):
-        return [self.major, sorted(self.req), sorted(self.elec)]
-
-    @staticmethod
-    def fields():
-        return ["Dept", "Required", "Electives"]
-
 class Student:
 
     def __init__(self, s_cwid, s_name, major):
@@ -152,17 +121,103 @@ class Instructor:
     def fields():
         return ["I_CWID", "I_name", "Dept", "Course", "Students"]
 
+class Major:
+    def __init__(self, major):
+        self.major = major
+        self.req = list()
+        self.elec = list()
+
+    def add_course(self, course, r_or_e):
+        if r_or_e == "R":
+            self.req.append(course)
+        elif r_or_e == "E":
+            self.elec.append(course)
+
+    def get_req(self, courses_completed):
+        result1 = []
+        result2 = []
+        for course in self.req:
+            if course not in courses_completed:
+                result1.append(course)
+        
+        for elective in self.elec:
+            if elective not in courses_completed:
+                result2.append(elective)
+            else:
+                result2 = "-"
+                break  
+        return (result1, result2)
+
+    def details(self):
+        return [self.major, sorted(self.req), sorted(self.elec)]
+
+    @staticmethod
+    def fields():
+        return ["Dept", "Required", "Electives"]
+
+class MajorTest(unittest.TestCase):
+    def test_init(self):
+        m1 = Major('SSW')
+        self.assertEqual(m1.major, "SSW")
+
+
+class StudentTest(unittest.TestCase):
+    def test_init(self):
+        s1 = Student("110", "Rucha, C", "SE")
+        self.assertEqual(s1.s_cwid, "110")
+        self.assertEqual(s1.s_name, "Rucha, C")
+        self.assertEqual(s1.major, "SE")
+        self.assertEqual(s1.courses, {})
+
+    def test_add_course(self):
+        s1 = Student("110", "Rucha, C", "SE")
+        s1.add_course("SSW 540", "A")
+        s1.add_course("SSW 810", "A")
+        s1.add_course("SSW 564", "A")
+        self.assertEqual(s1.courses, {'SSW 540': 'A','SSW 564': 'A','SSW 810': 'A'})
+        
+    def test_details(self):
+        s1 = Student("110", "Rucha, C", "SSW")
+        s2 = Student("210", "Ved, C", "CS")
+        s1.add_course("SSW 540", "A")
+        s1.add_course("SSW 810", "A")
+        s1.add_course("SSW 564", "A")
+        s2.add_course("CS 540", "A")
+        s2.add_course("CS 810", "A")
+        s2.add_course("CS 564", "A")
+        self.assertEqual(s1.details(), ['110', 'Rucha, C', 'SSW', ['SSW 540', 'SSW 564', 'SSW 810'], [], []])
+        self.assertEqual(s2.details(), ['210', 'Ved, C', 'CS', ['CS 540', 'CS 564', 'CS 810'], [], []])
+
+class InstructorTest(unittest.TestCase):
+    def test_init(self):
+        i1 = Instructor("310", "Chintan, M", "SSW")
+
+        self.assertEqual(i1.i_cwid, "310")
+        self.assertEqual(i1.i_name, "Chintan, M")
+        self.assertEqual(i1.dept, "SSW")
+        self.assertEqual(i1.courses, {})
+        self.assertEqual(i1.courses['test_key'], 0)
+
+    
+    def test_add_course(self):
+        i1 = Instructor("310", "Chintan, M", "SSW")
+        i1.add_course("SSW 540")
+        i1.add_course("SSW 810")
+        i1.add_course("SSW 540")
+        i1.add_course("SSW 810")
+        i1.add_course("SSW 540")
+        i1.add_course("SSW 564")
+        self.assertEqual(i1.courses, {'SSW 540': 3, 'SSW 564': 1, 'SSW 810': 2})
+
 class RepositoryTest(unittest.TestCase):
 
     def test_init(self):
-        path = "/Users/ruchabhatawadekar/Desktop/Stevens"
-        repository = Repository(path)
+        ob = Repository("/Users/ruchabhatawadekar/Desktop/Stevens")
         result1 = {'10103': ['10103', 'Baldwin, C', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687']], '10115': ['10115', 'Wyatt, X', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687']], '10172': ['10172', 'Forbes, I', ['SSW 555', 'SSW 567']], '10175': ['10175', 'Erickson, D', ['SSW 564', 'SSW 567', 'SSW 687']], '10183': ['10183', 'Chapman, O', ['SSW 689']], '11399': ['11399', 'Cordova, I', ['SSW 540']], '11461': ['11461', 'Wright, U', ['SYS 611', 'SYS 750', 'SYS 800']], '11658': ['11658', 'Kelly, P', ['SSW 540']], '11714': ['11714', 'Morton, A', ['SYS 611', 'SYS 645']], '11788': ['11788', 'Fuller, E', ['SSW 540']]}
         result2 = dict()
 
-        for keys, values in repository.students.items():
-            result2[keys] = values.details()
-
+        for student_cwid, values in ob.students.items():
+            result2[student_cwid] = values.details()
         self.assertEqual(result1, result2)
 
 def main():
